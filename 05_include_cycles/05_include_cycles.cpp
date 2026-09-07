5ccaa8249f12af584d2c1c8e3108883e393624bb
danilo <danilo.kozubovskyi@gmail.com>
2026-09-07 17:49:21 +0300

    Add project files.

---
+++ 05_include_cycles/05_include_cycles.cpp
@@ -0,0 +1,583 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	
+	
+
+
+	/*for (int i = 1; i <= 10; i++)
+	{
+
+		for (int j = 1; j < 10; j++)
+		{
+			cout << i << "*" << j << "=" << i * j << endl;
+		}
+		cout << endl;
+	}*/
+
+
+
+
+	//for (int i = 0; i < 3; i++)
+	//{
+	//	for (int j = 0; j < 4; j++)
+	//	{
+	//		cout << " +";
+	//	}cout << endl;
+	//}
+
+	////main
+	//for (int i = 0; i < 15; i++)
+	//{
+	//	for (int j = 0; j < 15; j++)
+	//	{
+	//		
+	//		if (i==j)
+	//		{
+	//			cout << "= ";
+	//		}
+	//		else
+	//		{
+	//			cout << "+ ";
+	//		}
+	//	}
+	//	cout << endl;
+	//}
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+	//int star_lines=1;
+	//int count_star;
+	//int lenght = 10;
+
+	//
+	//while (star_lines<=lenght)
+	//{
+	//	count_star = 1;
+	//	while (count_star<=lenght)
+	//	{
+	//		cout << "* ";
+	//		count_star++;
+	//	}
+	//	cout << endl;
+	//	star_lines++;
+	//}
+
+
+
+
+
+
+
+
+
+
+
+
+	//cout << endl;
+	//for (int i = 0; i < 15; i++)
+	//{
+	//	for (int j = 0; j < 15; j++)
+	//	{
+
+	//		if (i + j==15-1)
+	//		{
+	//			cout << "= ";
+	//		}
+	//		else
+	//		{
+	//			cout << "+ ";
+	//		}
+	//	}
+	//	cout << endl;
+	//}
+
+
+
+
+
+
+	/*int N = 10;
+
+	for (int i = 0; i < N; i++)
+	{
+
+		for (int j = 0; j < N; j++)
+		{
+			if (i>=j and i+j>=N-1)
+			{
+				cout << "|===|";
+			}
+			else
+			{
+				cout << "     ";
+			}
+			
+		}
+		cout << endl;
+	}
+
+
+
+	for (int i = 0; i < 7; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			cout << "|###|";
+		}
+		cout << endl;
+	}*/
+
+//практика ^^^^
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//a
+//int N = 10;
+//
+//for (int i = 0; i < N; i++)
+//{
+//
+//	for (int j = 0; j < N; j++)
+//	{
+//		if (i <= j and i + j <= N - 1)
+//		{
+//			cout << "= ";
+//		}
+//
+//		else if (i <= j and i + j >= N - 1)
+//		{
+//			cout << "= ";
+//		}
+//		else
+//		{
+//			cout << "+ ";
+//		}
+//
+//	}
+//	cout << endl;
+//}
+
+
+//б
+//int N = 10;
+//
+//for (int i = 0; i < N; i++)
+//{
+//
+//	for (int j = 0; j < N; j++)
+//	{
+//		if (i >= j and i + j <= N - 1)
+//		{
+//			cout << "= ";
+//		}
+//
+//		else if (i >= j and i + j >= N - 1)
+//		{
+//			cout << "= ";
+//		}
+//		else
+//		{
+//			cout << "+ ";
+//		}
+//
+//	}
+//	cout << endl;
+//}
+
+
+
+
+
+
+
+//в
+//int N = 10;
+//
+//for (int i = 0; i < N; i++)
+//{
+//
+//	for (int j = 0; j < N; j++)
+//	{
+//		if (i <= j and i + j <= N - 1)
+//		{
+//			cout << "= ";
+//		}
+//		else
+//		{
+//			cout << "+ ";
+//		}
+//
+//	}
+//	cout << endl;
+//}
+
+
+
+
+
+
+//г
+	//int N = 10;
+
+	//for (int i = 0; i < N; i++)
+	//{
+
+	//	for (int j = 0; j < N; j++)
+	//	{
+	//		if (i >= j and i + j >= N - 1)
+	//		{
+	//			cout << "= ";
+	//		}
+	//		else
+	//		{
+	//			cout << "+ ";
+	//		}
+
+	//	}
+	//	cout << endl;
+	//}
+
+
+
+
+
+
+//д
+//int N = 10;
+//
+//for (int i = 0; i < N; i++)
+//{
+//
+//	for (int j = 0; j < N; j++)
+//	{
+//		if (i <= j and i + j <= N - 1)
+//		{
+//			cout << "= ";
+//		}
+//
+//		else if (i >= j and i + j >= N - 1)
+//		{
+//			cout << "= ";
+//		}
+//		else
+//		{
+//			cout << "+ ";
+//		}
+//
+//	}
+//	cout << endl;
+//}
+
+
+
+
+
+
+
+
+//e
+//int N = 10;
+//
+//for (int i = 0; i < N; i++)
+//{
+//
+//	for (int j = 0; j < N; j++)
+//	{
+//		if (i <= j and i + j >= N - 1)
+//		{
+//			cout << "= ";
+//		}
+//
+//		else if (i >= j and i + j <= N - 1)
+//		{
+//			cout << "= ";
+//		}
+//		else
+//		{
+//			cout << "  ";
+//		}
+//
+//	}
+//	cout << endl;
+//}
+
+
+
+
+
+
+
+//Ж
+//int N = 10;
+//
+//for (int i = 0; i < N; i++)
+//{
+//
+//	for (int j = 0; j < N; j++)
+//	{
+//		if (i >= j and i + j <= N - 1)
+//		{
+//			cout << "= ";
+//		}
+//		else
+//		{
+//			cout << "+ ";
+//		}
+//
+//	}
+//	cout << endl;
+//}
+
+
+
+
+
+
+//з
+//int N = 10;
+//
+//for (int i = 0; i < N; i++)
+//{
+//
+//	for (int j = 0; j < N; j++)
+//	{
+//		if (i <= j and i + j >= N - 1)
+//		{
+//			cout << "= ";
+//		}
+//		else
+//		{
+//			cout << "+ ";
+//		}
+//
+//	}
+//	cout << endl;
+//}
+
+
+
+
+
+
+
+//и
+//int N = 10;
+//
+//for (int i = 0; i < N; i++)
+//{
+//
+//	for (int j = 0; j < N; j++)
+//	{
+//		if (i <= j and i + j <= N - 1)
+//		{
+//			cout << "= ";
+//		}
+//
+//		else if (i >= j and i + j <= N - 1)
+//		{
+//			cout << "= ";
+//		}
+//		else
+//		{
+//			cout << "+ ";
+//		}
+//
+//	}
+//	cout << endl;
+//}
+
+
+
+
+
+
+//к
+//int N = 10;
+//
+//for (int i = 0; i < N; i++)
+//{
+//
+//	for (int j = 0; j < N; j++)
+//	{
+//		if (i <= j and i + j >= N - 1)
+//		{
+//			cout << "= ";
+//		}
+//
+//		else if (i >= j and i + j >= N - 1)
+//		{
+//			cout << "= ";
+//		}
+//		else
+//		{
+//			cout << "+ ";
+//		}
+//
+//	}
+//	cout << endl;
+//}
+
+
+
+//1
+
+
+int n;
+cout << "enter amount of numbers to print: " << endl;
+cin >> n;
+
+for (int i = 1; i <= n; i++)
+{
+	for (int j = 0; j < i; j++)
+	{
+ 	cout << i;
+
+	}
+}
+
+
+
+//2
+
+
+
+//int N = 10;
+//
+//for (int i = 0; i < N; i++)
+//{
+//
+//	for (int j = 0; j < N; j++)
+//	{
+//		if (i==j)
+//		{
+//			cout << "0 ";
+//		}
+//		
+//		else
+//		{
+//			cout << "1 ";
+//		}
+//
+//	}
+//	cout << endl;
+//}
+
+
+
+
+//3
+
+//int ten = 10;
+//
+//for (int i = 0; i < ten; i++)
+//{
+//	cout << ten<<" ";
+//}
+//
+//int nine = 9;
+//cout << endl;
+//for (int i = 0; i < nine; i++)
+//{
+//	cout << nine << " ";
+//}
+//
+//int eight = 8;
+//cout << endl;
+//for (int i = 0; i < eight; i++)
+//{
+//	cout << eight << " ";
+//}
+//cout << endl;
+//cout << "1" << endl;
+//int suma = (ten * 10) + (nine * 9) + (eight * 8) + 1;
+//cout <<"suma=" << suma << endl;
+
+
+
+//4
+
+//
+//for (int i = 0; i < 15; i++)
+//{
+//	cout << i<<" ";
+//	if (i % 2 == 0) {
+//		cout<<
+//	}
+//}
+
+
+
+
+
+
+}
+
